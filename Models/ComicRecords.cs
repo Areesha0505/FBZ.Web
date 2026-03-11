8deb828e9dc7a35f9c987a1650c7e90d3bd2f01a
Areesha0505 <areesha1678@gmail.com>
2026-03-11 02:55:50 +0000

    Add project files.

---
+++ Models/ComicRecords.cs
@@ -0,0 +1,14 @@
+﻿namespace FBZ.Web.Models
+{
+    public class ComicRecord
+    {
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string DateOfPublication { get; set; }
+        public string ISBN { get; set; }
+        public string Edition { get; set; }
+        public string Languages { get; set; }
+        public string NameType { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FBZ.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
