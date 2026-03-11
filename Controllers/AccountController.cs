64a0cdc594092e215b3d05d6eadd34c2cf6fdf68
Areesha0505 <areesha1678@gmail.com>
2026-03-11 17:35:01 +0000

    Added user registration page and navigation links

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FBZ.Web.Models;
+
+namespace FBZ.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        static List<User> users = new List<User>();
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            users.Add(user);
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User loginUser)
+        {
+            var user = users.FirstOrDefault(u =>
+                u.Username == loginUser.Username &&
+                u.Password == loginUser.Password);
+
+            if (user != null)
+            {
+                HttpContext.Session.SetString("username", user.Username);
+                HttpContext.Session.SetString("role", user.Role);
+
+                return RedirectToAction("Index", "Comic");
+            }
+
+            ViewBag.Error = "Invalid login";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Comic");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using System.Text;
-
-Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();   
 
 var app = builder.Build();
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
 
-   app.MapControllerRoute(
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Comic}/{action=Index}/{id?}");
 
