0e6d53aabbfa67d8afe9cf90506d2017aced2699
Areesha0505 <areesha1678@gmail.com>
2026-05-12 21:28:03 +0100

    Added GoogleBookResult model for API integration

---
+++ Models/GoogleBookResult.cs
@@ -0,0 +1,11 @@
+﻿namespace FBZ.Web.Models
+{
+    public class GoogleBookResult
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Thumbnail { get; set; }
+    }
+}
